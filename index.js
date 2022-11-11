856000c6cfb012397ec833b988bf0877e3f9973e
ssuhacho <ssuhacho@gmail.com>
2022-11-11 13:46:32 -0600

    chrome extension make every img to pepe img

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a,i) => {
+        a.src='https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg'
+    })
+
+}, 500)
